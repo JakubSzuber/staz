e6f99a09ca1b59fdcc013dfe69447836a6fcd2f7
KamolR6 <kamilbar02@gmail.com>
2023-06-14 14:46:33 +0200

    added details about items, added buttons to navbar, changed ol to ul

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('members/details/<int:id>', views.details, name='details'),
     path('create/', views.create_record, name='create_record'),
     path('items/', views.ItemL, name='items'),
-    path('item/details/<int:id>', views.ItemDet, name='itemdetail'),
+    path('items/detailed/<int:id>', views.ItemDet, name='itemdetail'),
     path('testing/', views.testing, name='testing'),
 ]
