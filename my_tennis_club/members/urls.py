5a0c11ed67f1ead2dcf04a77f9d3d617c2fe5a98
KamolR6 <kamilbar02@gmail.com>
2023-06-14 14:12:41 +0200

    created 2 .html files & added urls

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
     path('create/', views.create_record, name='create_record'),
+    path('items/', views.Item, name='items'),
+    path('item/details/<int:id>', views.ItemDet, name='itemdetail'),
     path('testing/', views.testing, name='testing'),
 ]
